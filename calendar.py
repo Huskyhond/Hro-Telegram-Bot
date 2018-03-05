7f8ca41ac9bcb877da024c9f1aaf9094ab6611af
Niek Eichner <niek@twnkls.com>
2018-03-05 10:14:50 +0100

    Made calendar more generic

--- calendar.py
+++ calendar.py
@@ -10,7 +10,20 @@ class Calendar:
         return 'Use /thisweek to get this week\'s calendar\n Use /week <weekNr> to get the week calendar \n Use /nextweek to get the calendar of next week'
 
     def renderCalendarImage(self, weekNr):
-        url = "http://misc.hro.nl/roosterdienst/webroosters/CMI/kw3/" + weekNr + "/c/c00128.htm"
+        if classNumber < 1:
+            return False
+        quarter = 1
+        #w15 = s10
+        if weekNr > 15:
+            quarter = 4
+        elif weekNr > 25:
+            quarter = 1
+        elif  weekNr > 35:
+            quarter = 2
+        elif weekNr > 45:
+            quarter = 3
+
+        url = "http://misc.hro.nl/roosterdienst/webroosters/CMI/kw" + quarter + "/" + weekNr + "/c/c00"+ self.classNumber +".htm"
         req = urllib2.Request(url)
         try:
             resp = urllib2.urlopen(req)
@@ -50,8 +63,18 @@ class Calendar:
         else:
             bot.sendMessage(chat_id, text='De rooster maker mag wel is doorwerken...')
 
+    def classNumber(self, bot update args):
+        if len(args) > 0:
+                cn = int(args[0])
+                if cn < 1:
+                    bot.sendMessage(chat_id, text='We hebben een grapjas in de tent!')
+                    return
+                this.classNumber = str(cn)
+
     def __init__(self, dp):
         self.dp = dp
+        self.classNumber = 0
+        dp.addTelegramCommandHandler("class", self.classNumber)
         dp.addTelegramCommandHandler("week", self.week)
         dp.addTelegramCommandHandler("nextWeek", self.nextWeek)
         dp.addTelegramCommandHandler("nextweek", self.nextWeek)
